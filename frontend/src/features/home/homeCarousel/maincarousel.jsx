3bfe9e6dbc3adc423c25872d9e109b5a6273983b
Aayush Chauhan <aayushchauhan1916@gmail.com>
2024-07-30 10:35:50 +0530

    fixing HomeCarousel

--- frontend/src/features/home/homeCarousel/maincarousel.jsx
+++ frontend/src/features/home/homeCarousel/maincarousel.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
-import { maincarouseldata } from './maincarouseldata';
+import React from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import { maincarouseldata } from "./maincarouseldata";
+import { Link } from "react-router-dom";
 
 const Maincarousel = () => {
-    const items = maincarouseldata.map((item,index)=>{
-        return <img key={index} src={item.image} alt="{item.path}" className='cursor-pointer' role='presentation'/>
-    })
+  const items = maincarouseldata.map((item, index) => {
+    return (
+      <Link to="/women" key={index}>
+        <img
+          src={item.image}
+          alt="{item.path}"
+          className="cursor-pointer"
+          role="presentation"
+        />
+      </Link>
+    );
+  });
   return (
     <AliceCarousel
-        items={items}
-        // to disable arrow button
-        disableButtonsControls
-        autoPlay
-        autoPlayInterval={2000}
-        infinite
+      items={items}
+      // to disable arrow button
+      disableButtonsControls
+      autoPlay
+      autoPlayInterval={3000}
+      infinite
     />
-  )
-}
+  );
+};
 
-export default Maincarousel
+export default Maincarousel;
